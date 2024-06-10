5ae2e085ed61458ca0fccff0980910a565b02c98
CossacksUa <101990398+CossacksUa@users.noreply.github.com>
2024-06-10 21:58:17 +0300

    Добавьте файлы проекта.

---
+++ Lab19/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿using System.Windows.Forms;
+
+namespace Lab19
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnGetInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtOutput.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtOutput.Location = new System.Drawing.Point(10, 10);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(757, 588);
+            this.txtOutput.TabIndex = 0;
+            // 
+            // btnGetInfo
+            // 
+            this.btnGetInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnGetInfo.Location = new System.Drawing.Point(788, 521);
+            this.btnGetInfo.Name = "btnGetInfo";
+            this.btnGetInfo.Size = new System.Drawing.Size(156, 64);
+            this.btnGetInfo.TabIndex = 1;
+            this.btnGetInfo.Text = "Отримати інформацію";
+            this.btnGetInfo.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(956, 610);
+            this.Controls.Add(this.btnGetInfo);
+            this.Controls.Add(this.txtOutput);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Info Matherboard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtOutput;
+        private Button btnGetInfo;
+    }
+}
---
+++ Lab19/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management;
+using System.Windows.Forms;
+using OpenHardwareMonitor.Hardware;
+
+namespace Lab19
+{
+    public partial class Form1 : Form
+    {
+        private Computer computer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            computer = new Computer()
+            {
+                CPUEnabled = true,
+                GPUEnabled = true
+            };
+            computer.Open();
+
+            // Подключаем обработчик события для кнопки
+            btnGetInfo.Click += new EventHandler(btnGetInfo_Click);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Ініціалізація форми
+        }
+
+        private void btnGetInfo_Click(object sender, EventArgs e)
+        {
+            // Очищення виводу перед новим запитом
+            txtOutput.Clear();
+
+            AppendResult("Процесор:", GetHardwareInfo("Win32_Processor", "Name"));
+            AppendResult("Виробник процесора:", GetHardwareInfo("Win32_Processor", "Manufacturer"));
+            AppendResult("Опис процесора:", GetHardwareInfo("Win32_Processor", "Description"));
+            txtOutput.AppendText(Environment.NewLine);
+
+            AppendResult("Відеокарта:", GetHardwareInfo("Win32_VideoController", "Name"));
+            AppendResult("Видеопроцесор:", GetHardwareInfo("Win32_VideoController", "VideoProcessor"));
+            AppendResult("Версія драйверу відеокарти:", GetHardwareInfo("Win32_VideoController", "DriverVersion"));
+            AppendResult("Об’єм пам’яти відеокарти (в байтах):", GetHardwareInfo("Win32_VideoController", "AdapterRAM"));
+            txtOutput.AppendText(Environment.NewLine);
+
+            AppendTemperatureInfo();
+
+            txtOutput.AppendText(Environment.NewLine);
+            AppendResult("Назва DVD:", GetHardwareInfo("Win32_CDROMDrive", "Name"));
+            AppendResult("Буква DVD:", GetHardwareInfo("Win32_CDROMDrive", "Drive"));
+            txtOutput.AppendText(Environment.NewLine);
+
+            AppendResult("Жорстикий диск:", GetHardwareInfo("Win32_DiskDrive", "Caption"));
+            AppendResult("Об’єм жорстикого диску (в байтах):", GetHardwareInfo("Win32_DiskDrive", "Size"));
+            txtOutput.AppendText(Environment.NewLine);
+
+            AppendResult("Материнська плата:", GetHardwareInfo("Win32_BaseBoard", "Product"));
+            AppendResult("Виробник материнської плати:", GetHardwareInfo("Win32_BaseBoard", "Manufacturer"));
+            txtOutput.AppendText(Environment.NewLine);
+
+            AppendResult("Мережеве обладнання:", GetHardwareInfo("Win32_NetworkAdapter", "Name"));
+            AppendResult("MAC адреса:", GetHardwareInfo("Win32_NetworkAdapter", "MACAddress"));
+            txtOutput.AppendText(Environment.NewLine);
+
+            AppendResult("BIOS версія:", GetHardwareInfo("Win32_BIOS", "Version"));
+            AppendResult("BIOS виробник:", GetHardwareInfo("Win32_BIOS", "Manufacturer"));
+            AppendResult("BIOS опис:", GetHardwareInfo("Win32_BIOS", "Description"));
+            txtOutput.AppendText(Environment.NewLine);
+
+            
+        }
+
+        private List<string> GetHardwareInfo(string WIN32_Class, string ClassItemField)
+        {
+            List<string> result = new List<string>();
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM " + WIN32_Class);
+            try
+            {
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    result.Add(obj[ClassItemField]?.ToString().Trim() ?? "N/A");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return result;
+        }
+
+        private void AppendResult(string info, List<string> result)
+        {
+            if (!string.IsNullOrEmpty(info))
+            {
+                txtOutput.AppendText(info + Environment.NewLine);
+            }
+            if (result.Count > 0)
+            {
+                foreach (var item in result)
+                {
+                    txtOutput.AppendText(item + Environment.NewLine);
+                }
+            }
+        }
+
+        private void AppendTemperatureInfo()
+        {
+            foreach (var hardwareItem in computer.Hardware)
+            {
+                hardwareItem.Update();
+                foreach (var sensor in hardwareItem.Sensors)
+                {
+                    if (sensor.SensorType == SensorType.Temperature)
+                    {
+                        txtOutput.AppendText($"{sensor.Name}: {sensor.Value.GetValueOrDefault()} °C" + Environment.NewLine);
+                    }
+                }
+            }
+        }
+    }
+}
